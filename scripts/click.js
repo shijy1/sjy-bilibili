00f5da0117460152476dbe72b935535cef5d24b1
shijy1 <47687198+shijy1@users.noreply.github.com>
2019-08-11 21:16:23 +0800

    first commit

---
+++ scripts/click.js
@@ -0,0 +1,246 @@
+﻿var upgrate = document.getElementsByClassName('js_upgrate'),
+    dongtai = document.getElementsByClassName('js_dongtai');
+var btn_read = document.getElementsByClassName('btn1');
+//alert(btn_read[0].length);
+var btn_more = document.getElementsByClassName('btn2');
+var miaoshu = document.querySelectorAll('.bj .left > .content a > span');
+var pN = document.querySelectorAll('.bj .left > .content a .playNum');
+
+var qb = document.getElementsByClassName('ph_qb');
+var yc = document.getElementsByClassName('ph_yc');
+
+//竖导航
+var vas = document.querySelectorAll('.vMenu .fix .box a');
+var fix = document.querySelector('.Content > .vMenu .fix');
+var vclick = document.getElementsByClassName('vclick');
+var toTop = document.getElementsByClassName('toTop')[0];
+
+
+
+
+function fj() {
+
+    //var span = this.getElementsByTagName('span');
+    var index = this.getElementsByTagName('span')[0].className;
+    for (var j = 1; j < fj_week.length; j++) {
+        fj_week[0].style.color = '#000000';
+        //fj_week[0].getElementsByTagName('span')[0].style.display = 'none';
+        fj_week[j].innerText = fj_week[j].innerText.charAt(fj_week[j].innerText.length - 1);
+        //fj_week[j].getElementsByTagName('span')[0].style.display = 'none';
+        fj_week[j].style.color = '#000000';
+    }
+
+    if (this.innerText != '最新') {
+        this.innerText = '周' + this.innerText;
+    }
+    this.style.color = '#3B9ED5';
+    //alert(index);
+    for (var k = 0; k < fj_content.length; k++) {
+        fj_content[k].style.display = 'none';
+        //fj_content[k].classList.remove('contentShow');
+    }
+    for (var k in fj_content) {
+
+        //
+    }
+    var content = this.parentNode.parentNode.parentNode.parentNode.getElementsByClassName(index);
+    content[0].style.display = 'block';
+    //span[0].style.display = 'block';
+};
+
+
+//排行切换
+for (var i in qb) {
+    qb[i].onclick = phChange;
+}
+for (var i in yc) {
+    yc[i].onclick = phChange_a;
+}
+
+function phChange() {
+    var s = this.parentNode.parentNode.getElementsByClassName('main');
+    var d = this.parentNode.getElementsByTagName('span');
+    d[2].style.display = 'none';
+    d[1].style.display = 'block';
+    s[1].style.display = 'none';
+    s[0].style.display = 'block';
+}
+function phChange_a() {
+    var s = this.parentNode.parentNode.getElementsByClassName('main');
+    var d = this.parentNode.getElementsByTagName('span');
+    d[1].style.display = 'none';
+    d[2].style.display = 'block';
+    s[0].style.display = 'none';
+    s[1].style.display = 'block';
+}
+
+//播放数弹幕数悬浮下滑
+for (var i in miaoshu) {
+    // = pN[i].onmouseover 
+    miaoshu[i].onmouseover = xiaHua;
+    miaoshu[i].onmouseleave = xiaHua_a;
+}
+for (var i in pN) {
+    // = pN[i].onmouseover 
+    pN[i].onmouseover = function () { this.style.bottom = '-18px'; };
+    pN[i].onmouseleave = function () { this.style.bottom = '0px'; };
+}
+//播放数弹幕数悬浮下滑
+function xiaHua() {
+    this.parentNode.getElementsByClassName('playNum')[0].style.bottom = '-18px';
+}
+
+function xiaHua_a() {
+    this.parentNode.getElementsByClassName('playNum')[0].style.bottom = '0px';
+}
+
+
+//alert(bj_dongtai.length+','+bj_upgrate.length);
+for (var i in upgrate) {
+    //赋给事件的是函数名,  并没有参数的括号和参数,那样会出错--why？
+    //传参：onclick事件事件后定义一个方法, 我们可以在方法体中调用我们真正的方法并传入我们想要的参数,
+    upgrate[i].onclick = gengHuan;
+};
+for (var i in dongtai) {
+    dongtai[i].onclick = gengHuan_a;
+};
+
+//icon_read旋转
+for (var i in btn_read) {
+    btn_read[i].onmouseover = iRead;
+    btn_read[i].onmouseleave = iRead_a;
+}
+
+//icon_more分合
+for (var i in btn_more) {
+    btn_more[i].onmouseover = iMore;
+    btn_more[i].onmouseleave = iMore_a;
+}
+
+//icon_more分合
+function iMore() {
+    this.getElementsByTagName('div')[0].style.transform = 'translateX(3px)';
+    this.getElementsByTagName('span')[0].style.transform = 'translateX(-2px)';
+    this.getElementsByTagName('div')[0].style.transition = '0.3s ease';
+    this.getElementsByTagName('span')[0].style.transition = '0.3s ease';
+}
+function iMore_a() {
+    this.getElementsByTagName('div')[0].style.transform = 'translateX(0px)';
+    this.getElementsByTagName('span')[0].style.transform = 'translateX(0px)';
+}
+
+//icon_read旋转
+function iRead() {
+    this.getElementsByTagName('div')[0].style.transform = 'rotate(180deg)';
+    this.getElementsByTagName('div')[0].style.transition = '0.6s ease';
+}
+
+function iRead_a() {
+    this.getElementsByTagName('div')[0].style.transform = 'rotate(-180deg)';
+    this.getElementsByTagName('div')[0].style.transition = '0.6s ease';
+}
+
+//切换
+function gengHuan() {
+    var s = this.parentNode.parentNode.getElementsByClassName('content');
+    var d = this.parentNode.getElementsByTagName('span');
+    d[0].style.display = 'none';
+    d[1].style.display = 'block';
+    s[1].style.display = 'none';
+    s[2].style.display = 'block';
+};
+function gengHuan_a() {
+    var s = this.parentNode.parentNode.getElementsByClassName('content');
+    var d = this.parentNode.getElementsByTagName('span');
+    d[0].style.display = 'block';
+    d[1].style.display = 'none';
+    s[2].style.display = 'none';
+    s[1].style.display = 'block';
+};
+
+
+
+
+
+
+
+//***************竖导航事件实现******************
+//竖导航元素获取
+var vas = document.querySelectorAll('.vMenu .fix .box a'),
+    vbox = document.querySelector('.vMenu .fix .box'),
+    fix = document.querySelector('.Content > .vMenu .fix'),
+    vclick = document.getElementsByClassName('vclick'),
+    toTop = document.getElementsByClassName('toTop')[0];
+
+//获取模块数量，设置index
+var vlength = vas.length,
+    index = 0;
+
+//获取垂直导航的高度和浏览器视口的高度
+var slideH = fix.offsetHeight,
+    clientH = document.documentElement.clientHeight;
+//页面的高度减去导航地高度/2 = top
+var minus = Math.max((clientH - slideH), 0);//页面高度可能小于导航条高度
+var posH = minus / 2;
+//alert(posH);
+
+//监听页面滚动事件
+window.onscroll = function () {
+    var scrolltop = document.documentElement.scrollTop;
+    if (scrolltop > 243) {
+        fix.style.top = posH + 'px';//滚动超过243，使导航条居中显示
+        //鼠标滚轮滑动页面，导航条跟随变化
+        for (var j = 0; j < vclick.length; j++) {
+            //当该模块的顶部超过视口中线往上100px时，导航条跳转到对应部分
+            if ((vclick[j].offsetTop - scrolltop) < (clientH / 2 - 100)) {
+                dhChange(j);
+            }
+        }
+    } else {
+        fix.style.top = '234px';
+        dhChange();//不传参
+    }
+}
+
+//导航底部 toTop按钮
+toTop.onclick = function () {
+    document.documentElement.scrollTop = 0;
+};
+
+//事件委托函数
+var vasHandler = function (event) {
+    var e = event || window.event,
+        target = e.target || e.srcElement;
+    var vindex = target.getAttribute('data-vindex');
+    var pos = vclick[vindex].offsetTop;
+    switch (vindex) {
+        case '0':
+        case '2':
+        case '4':
+            pos = pos - 20;
+            break;
+    }
+    //导航条变化
+    dhChange(vindex);
+    //页面滚动
+    document.documentElement.scrollTop = pos;
+};
+
+//事件委托-点击使页面跳转到指定模块
+if (window.addEventListener) {
+    vbox.addEventListener('click', vasHandler, false);
+} else if (window.attachEvent) {
+    vbox.attachEvent('onclick', vasHandler);
+}
+
+//背景切换
+var dhChange = function (vindex) {
+    for (var i = 0; i < vas.length - 1; i++) {
+        vas[i].style.backgroundColor = '#fff';
+        vas[i].style.color = '#000';
+    }
+    vas[vindex].style.backgroundColor = '#00A1D6';
+    vas[vindex].style.color = '#fff';
+};
+
+
---
+++ scripts/qiehuan.js
@@ -0,0 +1,284 @@
+﻿//first切换
+var spans = document.querySelectorAll('.menu > .lists_content .left .first_spot span'),
+                first_spot = document.getElementsByClassName('first_spot')[0],
+                imgs = document.querySelectorAll('.menu > .lists_content .left .first_box img'),
+                first_box = document.getElementsByClassName('first_box')[0];
+//直播推荐动态切换
+var zbtj_box = document.getElementsByClassName('zbtj_box')[0],
+    zbtj_spot = document.getElementsByClassName('zbtj_bars')[0],
+    zbtj_imgs = document.querySelectorAll('.zhibo .right > .content .content3 .zbtj_box img'),
+    zbtj_spans = document.querySelectorAll('.zhibo .right > .content .content3 .zbtj_bars span');
+
+//番剧动态切换
+var fjdt_box = document.getElementsByClassName('fjdt_box')[0],
+    fjdt_spot = document.getElementsByClassName('fjdt_spot')[0],
+    fjdt_imgs = document.querySelectorAll('.fanju_dt .right .content .fjdt_box img'),
+    fjdt_spans = document.querySelectorAll('.fanju_dt .right .content .fjdt_spot span');
+//国创动态切换
+var gc_box = document.getElementsByClassName('gc_box')[0],
+    gc_spot = document.getElementsByClassName('gc_spot')[0],
+    gc_imgs = document.querySelectorAll('.guochuang .right .img .gc_box img'),
+    gc_spans = document.querySelectorAll('.guochuang .right .img .gc_spot span');
+
+//开启自动切换
+var timer = setInterval(autoTimer, 3000);
+var fjtimer = setInterval(fjdtTimer, 3000);
+var gctimer = setInterval(gcTimer, 3000);
+var zbtjtimer = setInterval(zbtjTimer, 3000);
+
+//鼠标移入取消定时器，移出开启
+for (var i in spans) {
+    spans[i].onmouseover = function () {
+        clearInterval(timer);
+    };
+    spans[i].onmouseleave = function () {
+        timer = setInterval(autoTimer, 3000);
+    };
+}
+
+//直播 鼠标移入取消定时器，移出开启
+for (var i in zbtj_spans) {
+    zbtj_spans[i].onmouseover = function () {
+        clearInterval(zbtjtimer);
+    };
+    zbtj_spans[i].onmouseleave = function () {
+        zbtjtimer = setInterval(zbtjTimer, 3000);
+    };
+}
+
+//番剧 鼠标移入取消定时器，移出开启
+for (var i in fjdt_spans) {
+    fjdt_spans[i].onmouseover = function () {
+        clearInterval(fjtimer);
+    };
+    fjdt_spans[i].onmouseleave = function () {
+        fjtimer = setInterval(fjdtTimer, 3000);
+    };
+}
+
+//国创 鼠标移入取消定时器，移出开启
+for (var i in gc_spans) {
+    gc_spans[i].onmouseover = function () {
+        clearInterval(gctimer);
+    };
+    gc_spans[i].onmouseleave = function () {
+        gctimer = setInterval(gcTimer, 3000);
+    };
+}
+
+//alert(fjdt_imgs[0].offsetWidth);
+//设置index
+var index = 0;
+var fjIndex = 0;
+var gcIndex = 0;
+var zbIndex = 0;
+var w = imgs[0].offsetWidth;
+var fjw = fjdt_imgs[0].offsetWidth;
+var gcw = gc_imgs[0].offsetWidth;
+var zbw = zbtj_imgs[0].offsetWidth;
+
+//自动切换
+function autoTimer() {
+    if (first_box.offsetLeft == -4 * w ) {
+        index = 0;
+    } else {
+        index++;
+    }
+    var leftChange = index * w;
+    first_box.style.left = -leftChange + 'px';
+    for (var j = 0; j < spans.length; j++) {
+        spans[j].style.background = 'url(../images/icons.png) 747px -790px';
+    }
+    spans[index].style.background = 'url(../images/icons.png) 747px -727px';
+}
+
+//直播推荐自动切换
+function zbtjTimer() {
+    if (zbtj_box.offsetLeft == -2 * zbw) {
+        zbIndex = 0;
+    } else {
+        zbIndex++;
+    }
+    var leftChange = zbIndex * zbw;
+    zbtj_box.style.left = -leftChange + 'px';
+    for (var j = 0; j < zbtj_spans.length; j++) {
+        zbtj_spans[j].style.width = '10px';
+        zbtj_spans[j].style.backgroundColor = '#FFFFFF';
+    }
+    zbtj_spans[zbIndex].style.width = '30px';
+    zbtj_spans[zbIndex].style.backgroundColor = '#EC568E';
+}
+
+//番剧自动切换
+function fjdtTimer() {
+    if (fjdt_box.offsetLeft == -2 * fjw) {
+        fjIndex = 0;
+    } else {
+        fjIndex++;
+    }
+    var leftChange = fjIndex * fjw;
+    fjdt_box.style.left = -leftChange + 'px';
+    for (var j = 0; j < fjdt_spans.length; j++) {
+        fjdt_spans[j].style.width = '10px';
+        fjdt_spans[j].style.backgroundColor = '#FFFFFF';
+    }
+    fjdt_spans[fjIndex].style.width = '30px';
+    fjdt_spans[fjIndex].style.backgroundColor = '#EC568E';
+}
+//国创自动切换
+function gcTimer() {
+    if (gc_box.offsetLeft == -gcw) {
+        gcIndex = 0;
+    } else {
+        gcIndex++;
+    }
+    var leftChange = gcIndex * gcw;
+    gc_box.style.left = -leftChange + 'px';
+    for (var j = 0; j < gc_spans.length; j++) {
+        gc_spans[j].style.width = '10px';
+        gc_spans[j].style.backgroundColor = '#FFFFFF';
+    }
+    gc_spans[gcIndex].style.width = '20px';
+    gc_spans[gcIndex].style.backgroundColor = '#EC568E';
+}
+
+//点击运行函数
+handler = function (event) {
+    var e = event || window.event,
+        target = e.target || e.srcElement;
+    var left = w;
+    var index = 0;
+    for (var j = 0; j < spans.length; j++) {
+        spans[j].style.background = 'url(../images/icons.png) 747px -790px';
+    }
+    switch (target.id) {
+        case "s1":
+            index = 0;
+            first_box.style.left = -index * left + 'px';
+            spans[index].style.background = 'url(../images/icons.png) 747px -727px';
+            break;
+        case "s2":
+            index += 1;
+            first_box.style.left = -index * left + 'px';
+            spans[index].style.background = 'url(../images/icons.png) 747px -727px';
+            break;
+        case "s3":
+            index += 2;
+            first_box.style.left = -index * left + 'px';
+            spans[index].style.background = 'url(../images/icons.png) 747px -727px';
+            break;
+        case "s4":
+            index += 3;
+            first_box.style.left = -index * left + 'px';
+            spans[index].style.background = 'url(../images/icons.png) 747px -727px';
+            break;
+        case "s5":
+            index += 4;
+            first_box.style.left = -index * left + 'px';
+            spans[index].style.background = 'url(../images/icons.png) 747px -727px';
+            break;
+    }
+
+}
+
+//zb运行函数
+zbHandler = function (event) {
+    var e = event || window.event,
+        target = e.target || e.srcElement;
+    var zbleft = -zbw, zbIndex = 0;
+    for (var j = 0; j < zbtj_spans.length; j++) {
+        zbtj_spans[j].style.width = '10px';
+        zbtj_spans[j].style.backgroundColor = '#FFFFFF';
+    }
+    switch (target.id) {
+        case "s1":
+            zbIndex = 0;
+            zbtj_box.style.left = zbIndex * zbleft + 'px';
+            zbtj_spans[zbIndex].style.width = '30px';
+            zbtj_spans[zbIndex].style.backgroundColor = '#EC568E';
+            break;
+        case "s2":
+            zbIndex += 1;
+            zbtj_box.style.left = zbIndex * zbleft + 'px';
+            zbtj_spans[zbIndex].style.width = '30px';
+            zbtj_spans[zbIndex].style.backgroundColor = '#EC568E';
+            break;
+        case "s3":
+            zbIndex += 2;
+            zbtj_box.style.left = zbIndex * zbleft + 'px';
+            zbtj_spans[zbIndex].style.width = '30px';
+            zbtj_spans[zbIndex].style.backgroundColor = '#EC568E';
+            break;
+    }
+}
+
+
+//fj运行函数
+fjHandler = function (event) {
+    var e = event || window.event,
+        target = e.target || e.srcElement;
+    var fjleft = -fjw, fjIndex = 0;
+    for (var j = 0; j < fjdt_spans.length; j++) {
+        fjdt_spans[j].style.width = '10px';
+        fjdt_spans[j].style.backgroundColor = '#FFFFFF';
+    }
+    switch (target.id) {
+        case "s1":
+            fjIndex = 0;
+            fjdt_box.style.left = fjIndex * fjleft + 'px';
+            fjdt_spans[fjIndex].style.width = '30px';
+            fjdt_spans[fjIndex].style.backgroundColor = '#EC568E';
+            break;
+        case "s2":
+            fjIndex += 1;
+            fjdt_box.style.left = fjIndex * fjleft + 'px';
+            fjdt_spans[fjIndex].style.width = '30px';
+            fjdt_spans[fjIndex].style.backgroundColor = '#EC568E';
+            break;
+        case "s3":
+            fjIndex += 2;
+            fjdt_box.style.left = fjIndex * fjleft + 'px';
+            fjdt_spans[fjIndex].style.width = '30px';
+            fjdt_spans[fjIndex].style.backgroundColor = '#EC568E';
+            break;
+    }
+}
+
+//gc执行函数
+gcHandler = function (event) {
+    var e = event || window.event,
+        target = e.target || e.srcElement;
+    var gcleft = -gcw, gcIndex = 0;
+    for (var j = 0; j < gc_spans.length; j++) {
+        gc_spans[j].style.width = '10px';
+        gc_spans[j].style.backgroundColor = '#FFFFFF';
+    }
+    switch (target.id) {
+        case "s1":
+            gcIndex = 0;
+            gc_box.style.left = gcIndex * gcleft + 'px';
+            gc_spans[gcIndex].style.width = '20px';
+            gc_spans[gcIndex].style.backgroundColor = '#EC568E';
+            break;
+        case "s2":
+            gcIndex += 1;
+            gc_box.style.left = gcIndex * gcleft + 'px';
+            gc_spans[gcIndex].style.width = '20px';
+            gc_spans[gcIndex].style.backgroundColor = '#EC568E';
+            break;
+    }
+}
+
+//委托
+if (window.addEventListener) {
+    first_spot.addEventListener('click', handler, false);
+    zbtj_spot.addEventListener('mouseover', zbHandler, false);
+    fjdt_spot.addEventListener('mouseover', fjHandler, false);
+    gc_spot.addEventListener('mouseover', gcHandler, false);
+} else if (window.attachEvent) {
+    first_spot.attachEvent('onclick', handler);
+    zbtj_spot.attachEvent('onmouseover', zbHandler);
+    fjdt_spot.attachEvent('onmouseover', fjHandler);
+    gc_spot.attachEvent('onmouseover', gcHandler);
+}
+
