c7cbd0de71612d6befd0c640c595d2ede7fd5640
shijy1 <47687198+shijy1@users.noreply.github.com>
2019-08-18 10:35:43 +0800

    add most mouseover content and event

--- scripts/over.js
+++ scripts/over.js
@@ -2,6 +2,8 @@
 var overAparent = document.getElementsByClassName('parentTop')[0];
 var topas = document.querySelectorAll('.parentTop ul li a');
 
+var indexList = document.querySelector('.menu > .lists');
+
 for (var i in overShow) {
     overShow[i].onmouseover = overdiv;
     overShow[i].onmouseout = outdiv;
@@ -46,4 +48,63 @@ if (window.addEventListener) {
 } else if (window.attachEvent) {
     overAparent.attachEvent('onmouseover', overHandler);
     overAparent.attachEvent('onmouseout', overHandler);
+}
+
+
+var indexOver = document.getElementsByClassName('index-over');
+
+//index mouseover
+indexovHandler = function (event) {
+    var e = event || window.event,
+        target = e.target || e.srcElement;
+    target.style.left = '5px';
+    this.style.display = 'block';
+    //target.firstElementChild.style.left = '0px';
+    //if (target.className == 'in') {
+    //    target.parentNode.style.left = '5px';
+    //    target.style.left = '0px';
+    //}
+};
+
+indexouHandler = function (event) {
+    var e = event || window.event,
+        target = e.target || e.srcElement;
+    target.style.left = '0px';
+    this.style.display = 'none';
+    //target.firstElementChild.style.left = '100px';
+    //if (target.className == 'in') {
+    //    target.parentNode.style.left = '5px';
+    //    target.style.left = '0px';
+    //}
+};
+
+//indexlist mouseover
+indexliiHandler = function (event) {
+    var e = event || window.event,
+        target = e.target || e.srcElement;
+    //alert(target);
+    if (target.className == 'overa') {
+        target.nextElementSibling.style.display = 'block';
+    }
+};
+
+indexlioHandler = function (event) {
+    var e = event || window.event,
+        target = e.target || e.srcElement;
+    if (target.className == 'overa') {
+        target.nextElementSibling.style.display = 'none';
+    }
+};
+
+
+if (window.addEventListener) {
+    indexList.addEventListener('mouseover', indexliiHandler, false);
+    indexList.addEventListener('mouseout', indexlioHandler, false);
+} else if (window.attachEvent) {
+    indexList.attachEvent('onmouseover', indexliHandler);
+}
+
+for (var i in indexOver) {
+    indexOver[i].addEventListener('mouseover', indexovHandler, false);
+    indexOver[i].addEventListener('mouseout', indexouHandler, false);
 }
